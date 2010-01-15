117b68a086d27d4aa14a902519e01937d7897ab9
cmarrin <cmarrin@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2010-01-15 23:54:00 +0000

    another fix to test mipmapping
    git-svn-id: https://cvs.khronos.org/svn/repos/registry/trunk/public/webgl@10075 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- sdk/demos/webkit/resources/utils3d.js
+++ sdk/demos/webkit/resources/utils3d.js
@@ -510,7 +510,7 @@ function doLoadImageTexture(ctx, image, texture)
     ctx.bindTexture(ctx.TEXTURE_2D, texture);
     ctx.texImage2D(ctx.TEXTURE_2D, 0, image);
     ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MAG_FILTER, ctx.LINEAR);
-    ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MIN_FILTER, ctx.LINEAR_MIPMAP_LINEAR);
+    ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MIN_FILTER, ctx.LINEAR);
     ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_S, ctx.CLAMP_TO_EDGE);
     ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_T, ctx.CLAMP_TO_EDGE);
     //ctx.generateMipmap(ctx.TEXTURE_2D)
