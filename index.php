b48ca876352f6cc1d8a01c27c8628a3dd0c3a3e1
kbr_google <kbr_google@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2011-01-27 20:42:24 +0000

    Added top-level index.php, copied and simplified from the OpenGL ES registry, on request from Jon Leech.
    git-svn-id: https://cvs.khronos.org/svn/repos/registry/trunk/public/webgl@13709 e7fa87d3-cd2b-0410-9028-fcbf551c1848

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("../../_inc/doctype.txt"); ?>
+<head>
+<title>Khronos WebGL API Registry</title>
+<?php include("../../_inc/registry/registry_head.txt"); ?>
+</head>
+<body id="developers" class="registry">
+<?php include("../../_inc/registry/registry_body.txt"); ?>
+<h1 class="pagetitle">Khronos WebGL API Registry</h1> <!--	top left shadow-->
+<div id="mainformat">
+
+
+<h1 style="text-align:center"> Khronos WebGL API Registry </h1>
+
+<p> The WebGL API registry contains specifications of the core API;
+    specifications of Khronos- and vendor-approved WebGL extensions;
+    IDL files corresponding to the specifications; and other related
+    documentation.
+
+<h6> WebGL Core API Specification, IDL, and Documentation </h6>
+
+<ul>
+<li> <a href="specs/latest">WebGL current draft specification</a>.
+     </li>
+<li> <a href="specs/latest/webgl.idl"> webgl.idl </a>
+     Web IDL description of the WebGL API. </li>
+</ul>
+
+<h6> <a name="otherextspecs"></a>
+     WebGL Extensions</h6>
+<ul>
+<li> <a href="extensions/">WebGL extension registry</a> </li>
+</ul>
+<?php include("../../_inc/registry/registry_footer.txt"); ?>
+</body>  <!-- END pagewrapper -->
+</html>
