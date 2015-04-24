be9c5f2a56176d00a6830fe303221d106a5dd36d
He Yunchao <yunchao.he@intel.com>
2015-04-24 16:20:23 +0800

    move all files under sdk/tests/conformance/resources/ to sdk/tests/resource/

--- sdk/tests/resources/gl-object-get-calls.js
+++ sdk/tests/resources/gl-object-get-calls.js
@@ -465,7 +465,7 @@ testInvalidArgument(
 debug("");
 debug("Test getUniform with all variants of data types");
 // Boolean uniform variables
-var boolProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/boolUniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+var boolProgram = wtu.loadProgramFromFile(gl, "/resources/boolUniformShader.vert", "/resources/noopUniformShader.frag");
 shouldBe('gl.getProgramParameter(boolProgram, gl.LINK_STATUS)', 'true');
 var bvalLoc = gl.getUniformLocation(boolProgram, "bval");
 var bval2Loc = gl.getUniformLocation(boolProgram, "bval2");
@@ -482,7 +482,7 @@ shouldBe('gl.getUniform(boolProgram, bval2Loc)', '[true, false]');
 shouldBe('gl.getUniform(boolProgram, bval3Loc)', '[true, false, true]');
 shouldBe('gl.getUniform(boolProgram, bval4Loc)', '[true, false, true, false]');
 // Integer uniform variables
-var intProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/intUniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+var intProgram = wtu.loadProgramFromFile(gl, "/resources/intUniformShader.vert", "/resources/noopUniformShader.frag");
 shouldBe('gl.getProgramParameter(intProgram, gl.LINK_STATUS)', 'true');
 var ivalLoc = gl.getUniformLocation(intProgram, "ival");
 var ival2Loc = gl.getUniformLocation(intProgram, "ival2");
@@ -499,7 +499,7 @@ shouldBe('gl.getUniform(intProgram, ival2Loc)', '[2, 3]');
 shouldBe('gl.getUniform(intProgram, ival3Loc)', '[4, 5, 6]');
 shouldBe('gl.getUniform(intProgram, ival4Loc)', '[7, 8, 9, 10]');
 // Float uniform variables
-var floatProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/floatUniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+var floatProgram = wtu.loadProgramFromFile(gl, "/resources/floatUniformShader.vert", "/resources/noopUniformShader.frag");
 shouldBe('gl.getProgramParameter(floatProgram, gl.LINK_STATUS)', 'true');
 var fvalLoc = gl.getUniformLocation(floatProgram, "fval");
 var fval2Loc = gl.getUniformLocation(floatProgram, "fval2");
@@ -516,7 +516,7 @@ shouldBe('gl.getUniform(floatProgram, fval2Loc)', '[12, 13]');
 shouldBe('gl.getUniform(floatProgram, fval3Loc)', '[14, 15, 16]');
 shouldBe('gl.getUniform(floatProgram, fval4Loc)', '[17, 18, 19, 20]');
 // Sampler uniform variables
-var samplerProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/noopUniformShader.vert", "../../conformance/resources/samplerUniformShader.frag");
+var samplerProgram = wtu.loadProgramFromFile(gl, "/resources/noopUniformShader.vert", "/resources/samplerUniformShader.frag");
 shouldBe('gl.getProgramParameter(samplerProgram, gl.LINK_STATUS)', 'true');
 var s2DValLoc = gl.getUniformLocation(samplerProgram, "s2D");
 var sCubeValLoc = gl.getUniformLocation(samplerProgram, "sCube");
@@ -527,7 +527,7 @@ wtu.glErrorShouldBe(gl, gl.NO_ERROR);
 shouldBe('gl.getUniform(samplerProgram, s2DValLoc)', '0');
 shouldBe('gl.getUniform(samplerProgram, sCubeValLoc)', '1');
 // Matrix uniform variables
-var matProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/matUniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+var matProgram = wtu.loadProgramFromFile(gl, "/resources/matUniformShader.vert", "/resources/noopUniformShader.frag");
 shouldBe('gl.getProgramParameter(matProgram, gl.LINK_STATUS)', 'true');
 var mval2Loc = gl.getUniformLocation(matProgram, "mval2");
 var mval3Loc = gl.getUniformLocation(matProgram, "mval3");
@@ -542,7 +542,7 @@ shouldBe('gl.getUniform(matProgram, mval3Loc)', '[5, 6, 7, 8, 9, 10, 11, 12, 13]
 shouldBe('gl.getUniform(matProgram, mval4Loc)', '[14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]');
 if (contextVersion > 1) {
   // Unsigned Integer uniform variables
-  var uintProgram = wtu.loadProgramFromFile(gl, "../../conformance/resources/uintUniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+  var uintProgram = wtu.loadProgramFromFile(gl, "/resources/uintUniformShader.vert", "/resources/noopUniformShader.frag");
   shouldBe('gl.getProgramParameter(uintProgram, gl.LINK_STATUS)', 'true');
   var uvalLoc = gl.getUniformLocation(uintProgram, "uval");
   var uval2Loc = gl.getUniformLocation(uintProgram, "uval2");
@@ -559,7 +559,7 @@ if (contextVersion > 1) {
   shouldBe('gl.getUniform(uintProgram, uval3Loc)', '[4, 5, 6]');
   shouldBe('gl.getUniform(uintProgram, uval4Loc)', '[7, 8, 9, 10]');
   // Matrix uniform variables for WebGL 2
-  var matForWebGL2Program = wtu.loadProgramFromFile(gl, "../../conformance/resources/matForWebGL2UniformShader.vert", "../../conformance/resources/noopUniformShader.frag");
+  var matForWebGL2Program = wtu.loadProgramFromFile(gl, "/resources/matForWebGL2UniformShader.vert", "/resources/noopUniformShader.frag");
   shouldBe('gl.getProgramParameter(matForWebGL2Program, gl.LINK_STATUS)', 'true');
   var mval2x3Loc = gl.getUniformLocation(matProgram, "mval2x3");
   var mval2x4Loc = gl.getUniformLocation(matProgram, "mval2x4");
@@ -582,7 +582,7 @@ if (contextVersion > 1) {
   shouldBe('gl.getUniform(matForWebGL2Program, mval4x2Loc)', '[33, 34, 35, 36, 37, 38, 39, 40]');
   shouldBe('gl.getUniform(matForWebGL2Program, mval4x3Loc)', '[41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]');
   // Sampler uniform variables for WebGL2
-  var samplerForWebGL2Program = wtu.loadProgramFromFile(gl, "../../conformance/resources/noopUniformShader.vert", "../../conformance/resources/samplerForWebGL2UniformShader.frag");
+  var samplerForWebGL2Program = wtu.loadProgramFromFile(gl, "/resources/noopUniformShader.vert", "/resources/samplerForWebGL2UniformShader.frag");
   shouldBe('gl.getProgramParameter(samplerForWebGL2Program, gl.LINK_STATUS)', 'true');
   var s3DValLoc = gl.getUniformLocation(samplerForWebGL2Program, "s3D");
   var s2DArrayValLoc = gl.getUniformLocation(samplerForWebGL2Program, "s2DArray");
