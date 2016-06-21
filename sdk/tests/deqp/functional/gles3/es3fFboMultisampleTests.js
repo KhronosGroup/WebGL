06394e2cc74c23eb33b56d13aaa9dfc87b42e9fc
He Yunchao <yunchao.he@intel.com>
2016-06-21 13:41:55 +0800

    Fix bugs in fbomultisample.html
    Multi-sample floating-point color buffers are not supported.
    Skip tests against those color formats.

--- sdk/tests/deqp/functional/gles3/es3fFboMultisampleTests.js
+++ sdk/tests/deqp/functional/gles3/es3fFboMultisampleTests.js
@@ -305,13 +305,14 @@ var DE_ASSERT = function(x) {
             gl.R8,
 
             // gl.EXT_color_buffer_float
-            gl.RGBA32F,
-            gl.RGBA16F,
-            gl.R11F_G11F_B10F,
-            gl.RG32F,
-            gl.RG16F,
-            gl.R32F,
-            gl.R16F
+            // Multi-sample floating-point color buffers are not supported, see https://www.khronos.org/registry/webgl/extensions/EXT_color_buffer_float/
+            // gl.RGBA32F,
+            // gl.RGBA16F,
+            // gl.R11F_G11F_B10F,
+            // gl.RG32F,
+            // gl.RG16F,
+            // gl.R32F,
+            // gl.R16F
         ];
 
         /** @const {Array<number>} */ var depthStencilFormats = [
