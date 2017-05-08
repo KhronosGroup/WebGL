cb5e87976cd41498c2a47eee904b6c7d863dcdbf
Reid Beels <mail@reidbeels.com>
2017-05-08 11:20:04 -0700

    Replace Safari troubleshooting URL to fix broken link
    The page I chose as a replacement isn't perfect, but it's the best I could find on either apple.com or webkit.org that gets the message across that Safari hides its WebGL settings under "Security".
    
    The previous URL (http://www.webkit.org/blog/919/webgl-draft-specification-now-available/) now leads to a 404.

--- other/get.webgl.org/troubleshooting/DoNotCopyOrLinkThisFileElseYouWillNotGetAutoUpdatedHelpForYourUsers.js
+++ other/get.webgl.org/troubleshooting/DoNotCopyOrLinkThisFileElseYouWillNotGetAutoUpdatedHelpForYourUsers.js
@@ -247,11 +247,11 @@ var BrowserDetect = {
         },
         "Mac": {
           upgradeUrl: "http://www.webkit.org/",
-          troubleshootingUrl: "http://www.webkit.org/blog/919/webgl-draft-specification-now-available/"
+          troubleshootingUrl: "https://support.apple.com/kb/PH21426"
         }
       },
       upgradeUrl: "http://www.webkit.org/",
-      troubleshootingUrl: "http://www.webkit.org/blog/919/webgl-draft-specification-now-available/"
+      troubleshootingUrl: "https://support.apple.com/kb/PH21426"
     },
     "Explorer": {
       upgradeUrl: "http://www.microsoft.com/ie",
