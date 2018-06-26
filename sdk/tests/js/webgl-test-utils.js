cd43e6b0ea4e4cbe16c31d24eacb84420f1e79e0
Jeff Gilbert <jdashg@gmail.com>
2018-06-25 18:46:40 -0700

    Add wtu.dispatchTask for use instead of waitForComposite. (#2661)
    Texture upload tests in particular take a long time because they run a max of
    60 tests per second, because they rely on waitForComposite to yield execution.
    waitForComposite should only be used when necessary. Use dispatchTask if we
    want to yield to the event loop.
    
    Before:
    all/conformance/textures/canvas (Passed: 3880/3880 in 48.68 seconds)
    conformance/textures/canvas/tex-2d-rgb-rgb-unsigned_byte.html (Passed: 1122/1122 in 14113.0 ms)
    
    After:
    all/conformance/textures/canvas (Passed: 3880/3880 in 8.90 seconds)
    conformance/textures/canvas/tex-2d-rgb-rgb-unsigned_byte.html (Passed: 1122/1122 in 2097.0 ms)

--- sdk/tests/js/webgl-test-utils.js
+++ sdk/tests/js/webgl-test-utils.js
@@ -2865,6 +2865,35 @@ var waitForComposite = function(callback) {
   countDown();
 };
 
+var setZeroTimeout = (function() {
+  // See https://dbaron.org/log/20100309-faster-timeouts
+
+  var timeouts = [];
+  var messageName = "zero-timeout-message";
+
+  // Like setTimeout, but only takes a function argument.  There's
+  // no time argument (always zero) and no arguments (you have to
+  // use a closure).
+  function setZeroTimeout(fn) {
+      timeouts.push(fn);
+      window.postMessage(messageName, "*");
+  }
+
+  function handleMessage(event) {
+      if (event.source == window && event.data == messageName) {
+          event.stopPropagation();
+          if (timeouts.length > 0) {
+              var fn = timeouts.shift();
+              fn();
+          }
+      }
+  }
+
+  window.addEventListener("message", handleMessage, true);
+
+  return setZeroTimeout;
+})();
+
 /**
  * Runs an array of functions, yielding to the browser between each step.
  * If you want to know when all the steps are finished add a last step.
@@ -3139,6 +3168,7 @@ var API = {
   clearAndDrawUnitQuad: clearAndDrawUnitQuad,
   clearAndDrawIndexedQuad: clearAndDrawIndexedQuad,
   comparePixels: comparePixels,
+  dispatchTask: setZeroTimeout,
   displayImageDiff: displayImageDiff,
   drawUnitQuad: drawUnitQuad,
   drawIndexedQuad: drawIndexedQuad,
