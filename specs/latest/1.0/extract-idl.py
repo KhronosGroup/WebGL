677a0e8fef00c34f02232c5f08a6ceeb3147a209
Ken Russell <kbrussel@alum.mit.edu>
2018-07-20 17:51:35 -0700

    Add license to webgl.idl and webgl2.idl. (#2674)
    Use the same MIT license that's used throughout the WebGL SDK, as it
    seems a better choice than Khronos' specification license.
    
    Remove obsolete comments referring to Khronos' typed array spec; it
    was folded into the ECMAScript specification some time ago.
    
    Fixes #2668 .

--- specs/latest/1.0/extract-idl.py
+++ specs/latest/1.0/extract-idl.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python
 
+from datetime import date
+from string import Template
 import sys
 import html5lib
+
+LICENSE = """
+// Copyright (c) $YEAR The Khronos Group Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and/or associated documentation files (the
+// "Materials"), to deal in the Materials without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Materials, and to
+// permit persons to whom the Materials are furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Materials.
+//
+// THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+
+"""
 
 htmlfilename = sys.argv[1]
 htmlfile = open(htmlfilename)
@@ -34,4 +60,5 @@ def elementTextContent(el):
 
 preList = doc.getElementsByTagName("pre")
 idlList = [elementTextContent(p) for p in preList if elementHasClass(p, "idl") ]
-print "\n\n".join(idlList)
+licenseTemplate = Template(LICENSE)
+print licenseTemplate.substitute(YEAR=date.today().year) + "\n\n".join(idlList)
