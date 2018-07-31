21bf7a8843da78e6cdf066f3165c1b8f7905d04b
Olli Etuaho <olli.etuaho@gmail.com>
2018-07-31 11:09:47 +0300

    Add more WEBGL_multiview tests (#2679)
    Add more WEBGL_multiview tests
    
    This adds tests for different features that interact with
    WEBGL_multiview:
    
    1. Multiple draw buffers.
    2. Using a flat varying in a multiview shader (this covers a bug that
       was recently worked around in ANGLE).
    3. Instanced draw calls. Multiview can be implemented by leveraging
       instancing together with some other GL / D3D11 features, so it's
       important to test this.
    
    All of these tests pass when run against Chrome Canary with WebGL
    draft extensions enabled.

--- sdk/tests/js/tests/webgl_multiview_util.js
+++ sdk/tests/js/tests/webgl_multiview_util.js
@@ -35,6 +35,52 @@ function createTextureWithNearestFiltering(target)
     return texture;
 }
 
+// Write a transformation matrix to elements of floatArray starting from index.
+// The matrix transforms a unit square (-1 to 1) to a rectangle with the width scaleX and the left edge at offsetX.
+function setupTranslateAndScaleXMatrix(floatArray, index, scaleX, offsetX)
+{
+    // x position is transformed according to this equation: scaleX * x0 + translateX = offsetX
+    // By substituting x0 with -1 (unit square x value for the left edge), we get the following:
+    let translateX = offsetX + scaleX;
+
+    floatArray[index] = scaleX;
+    floatArray[index + 1] = 0.0;
+    floatArray[index + 2] = 0.0;
+    floatArray[index + 3] = 0.0;
+
+    floatArray[index + 4] = 0.0;
+    floatArray[index + 5] = 1.0;
+    floatArray[index + 6] = 0.0;
+    floatArray[index + 7] = 0.0;
+
+    floatArray[index + 8] = 0.0;
+    floatArray[index + 9] = 0.0;
+    floatArray[index + 10] = 1.0;
+    floatArray[index + 11] = 0.0;
+
+    floatArray[index + 12] = translateX;
+    floatArray[index + 13] = 0.0;
+    floatArray[index + 14] = 0.0;
+    floatArray[index + 15] = 1.0;
+}
+
+// Check the currently bound read framebuffer with dimensions <width> x <height>.
+// The framebuffer should be divided into <strips> equally wide vertical strips, with the one indicated by
+// <coloredStripIndex> colored with <expectedStripColor>. The rest of the framebuffer should be colored transparent black.
+// A two pixel wide region at each edge of the colored region is left unchecked to allow for some tolerance for rasterization.
+function checkVerticalStrip(width, height, strips, coloredStripIndex, expectedStripColor, framebufferDescription)
+{
+    let colorRegionLeftEdge = (width / strips) * coloredStripIndex;
+    let colorRegionRightEdge = (width / strips) * (coloredStripIndex + 1);
+    if (coloredStripIndex > 0) {
+        wtu.checkCanvasRect(gl, 0, 0, colorRegionLeftEdge - 1, height, [0, 0, 0, 0], 'the left edge of ' + framebufferDescription + ' should be untouched');
+    }
+    if (coloredStripIndex < strips - 1) {
+        wtu.checkCanvasRect(gl, colorRegionRightEdge + 1, 0, width - colorRegionRightEdge - 1, height, [0, 0, 0, 0], 'the right edge of ' + framebufferDescription + ' should be untouched');
+    }
+    wtu.checkCanvasRect(gl, colorRegionLeftEdge + 1, 0, colorRegionRightEdge - colorRegionLeftEdge - 2, height, expectedStripColor, 'a thin strip in ' + framebufferDescription + ' should be colored ' + expectedStripColor);
+}
+
 function getMultiviewPassthroughVertexShader(views) {
     let shaderCode = ['#version 300 es',
     '#extension GL_OVR_multiview : require',
@@ -101,6 +147,97 @@ function getMultiviewColorFragmentShader() {
     '                        ((mask & 2u) != 0u) ? 1.0 : 0.0,',
     '                        ((mask & 1u) != 0u) ? 1.0 : 0.0,',
     '                        1.0);',
+    '}'].join('\n');
+}
+
+function getMultiviewColorFragmentShaderForDrawBuffers(drawBuffers) {
+    let shaderCode = ['#version 300 es',
+    '#extension GL_OVR_multiview : require',
+    'precision highp float;',
+
+    'out vec4 my_FragColor[$(drawBuffers)];',
+
+    'void main() {',
+    '    uint mask;'];
+
+    for (let i = 0; i < drawBuffers; ++i) {
+        shaderCode.push(wtu.replaceParams('    mask = gl_ViewID_OVR + $(i)u;', {'i': i + 1}));
+        shaderCode.push(wtu.replaceParams('    my_FragColor[$(i)] = vec4(((mask & 4u) != 0u) ? 1.0 : 0.0,', {'i': i}));
+        shaderCode.push('                           ((mask & 2u) != 0u) ? 1.0 : 0.0,');
+        shaderCode.push('                           ((mask & 1u) != 0u) ? 1.0 : 0.0,');
+        shaderCode.push('                           1.0);');
+    }
+    shaderCode.push('}');
+    shaderCode = shaderCode.join('\n');
+    return wtu.replaceParams(shaderCode, {'drawBuffers' : drawBuffers});
+}
+
+function getMultiviewFlatVaryingVertexShader(views) {
+    let shaderCode = ['#version 300 es',
+    '#extension GL_OVR_multiview : require',
+
+    'layout(num_views = $(num_views)) in;',
+
+    'in vec4 a_position;',
+    'flat out int testVarying;',
+
+    'void main() {',
+    '    gl_Position = a_position;',
+    '    testVarying = int(gl_ViewID_OVR);',
+    '}'].join('\n');
+    return wtu.replaceParams(shaderCode, {'num_views': views});
+}
+
+function getMultiviewFlatVaryingFragmentShader() {
+    return ['#version 300 es',
+    '#extension GL_OVR_multiview : require',
+    'precision highp float;',
+
+    'flat in int testVarying;',
+    'out vec4 my_FragColor;',
+
+    'void main() {',
+    '    int mask = testVarying + 1;',
+    '    my_FragColor = vec4(((mask & 4) != 0) ? 1.0 : 0.0,',
+    '                        ((mask & 2) != 0) ? 1.0 : 0.0,',
+    '                        ((mask & 1) != 0) ? 1.0 : 0.0,',
+    '                        1.0);',
+    '}'].join('\n');
+}
+
+function getMultiviewInstancedVertexShader(views) {
+    let shaderCode = ['#version 300 es',
+    '#extension GL_OVR_multiview : require',
+
+    'layout(num_views = $(num_views)) in;',
+
+    'in vec4 a_position;',
+    'out vec4 color;',
+
+    'void main() {',
+    '    vec4 pos = a_position;',
+    "    // Transform the quad to a thin vertical strip that's offset along the x axis according to the view id and instance id.",
+    '    pos.x = (pos.x * 0.5 + 0.5 + float(gl_ViewID_OVR) + float(gl_InstanceID)) * 2.0 / ($(num_views).0 * 2.0) - 1.0;',
+    '    int mask = gl_InstanceID + 1;',
+    '    color = vec4(((mask & 4) != 0) ? 1.0 : 0.0,',
+    '                 ((mask & 2) != 0) ? 1.0 : 0.0,',
+    '                 ((mask & 1) != 0) ? 1.0 : 0.0,',
+    '                 1.0);',
+    '    gl_Position = pos;',
+    '}'].join('\n');
+    return wtu.replaceParams(shaderCode, {'num_views': views});
+}
+
+function getInstanceColorFragmentShader() {
+    return ['#version 300 es',
+    '#extension GL_OVR_multiview : require',
+    'precision highp float;',
+
+    'in vec4 color;',
+    'out vec4 my_FragColor;',
+
+    'void main() {',
+    '    my_FragColor = color;',
     '}'].join('\n');
 }
 
