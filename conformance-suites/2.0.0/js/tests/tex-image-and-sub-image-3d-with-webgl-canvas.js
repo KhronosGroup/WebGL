7fd63b943aa7af1943de5adc0527fb5ff085a317
Jeff Gilbert <jdashg@gmail.com>
2019-02-13 17:49:53 -0800

    Backport #2661 (wtu.dispatchTask) to snapshots (#2803)
    * Backport #2661 (wtu.dispatchTask) to 2.0.0.
    
    This should improve run times.
    
    * Also backport wtu.dispatchTask to 1.0.3.

--- conformance-suites/2.0.0/js/tests/tex-image-and-sub-image-3d-with-webgl-canvas.js
+++ conformance-suites/2.0.0/js/tests/tex-image-and-sub-image-3d-with-webgl-canvas.js
@@ -186,7 +186,7 @@ function generateTest(internalFormat, pixelFormat, pixelType, prologue, resource
                             return;
                         }
                     }
-                    wtu.waitForComposite(runNextTest);
+                    wtu.dispatchTask(runNextTest);
                 }
                 runNextTest();
             });
